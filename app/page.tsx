c38dc369ca61eb1800a77fdcde35f586c2cf4bd4
Rauz46 <vivin.raj@caimue.com>
2026-01-21 16:16:30 +0530

    banner update

--- app/page.tsx
+++ app/page.tsx
@@ -74,14 +74,14 @@ export default function Home() {
       <section className="min-h-screen flex flex-col pt-32 pb-20 relative overflow-hidden">
         {/* Background Animation */}
         <BackgroundGradientAnimation
-          gradientBackgroundStart="rgb(109, 40, 217)"
-          gradientBackgroundEnd="rgb(190, 24, 93)"
-          firstColor="109, 40, 217"
-          secondColor="219, 39, 119"
-          thirdColor="124, 58, 237"
-          fourthColor="190, 24, 93"
-          fifthColor="130, 24, 67"
-          pointerColor="219, 39, 119"
+          gradientBackgroundStart="rgb(124, 58, 237)"
+          gradientBackgroundEnd="rgb(219, 39, 119)"
+          firstColor="124, 58, 237"
+          secondColor="236, 72, 153"
+          thirdColor="139, 92, 246"
+          fourthColor="219, 39, 119"
+          fifthColor="161, 30, 84"
+          pointerColor="236, 72, 153"
           containerClassName="absolute inset-0 z-0"
         />
 
@@ -97,9 +97,9 @@ export default function Home() {
           <div className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/4 w-[400px] h-[400px] rounded-full bg-gradient-radial from-fuchsia-400/15 to-transparent" />
 
           {/* Organic Blob Shapes - Restored Vibrancy */}
-          <div className="absolute -top-20 -right-20 w-[500px] h-[500px] bg-purple-600/15 rounded-full blur-[100px]" />
-          <div className="absolute -bottom-20 -left-20 w-[600px] h-[600px] bg-indigo-700/15 rounded-full blur-[120px]" />
-          <div className="absolute top-1/3 left-1/4 w-[400px] h-[400px] bg-violet-600/15 rounded-full blur-[80px]" />
+          <div className="absolute -top-20 -right-20 w-[500px] h-[500px] bg-purple-500/15 rounded-full blur-[100px]" />
+          <div className="absolute -bottom-20 -left-20 w-[600px] h-[600px] bg-indigo-600/15 rounded-full blur-[120px]" />
+          <div className="absolute top-1/3 left-1/4 w-[400px] h-[400px] bg-violet-500/15 rounded-full blur-[80px]" />
 
           {/* Decorative Stars and Sparkles */}
           <div className="absolute top-20 right-1/4 text-white/40 text-4xl">✦</div>
