759866b3d9987d94048a34f6a9fa99c05918f231
Rauz46 <vivin.raj@caimue.com>
2026-02-04 18:51:00 +0530

    scroll2

--- components/ui/ScrollIndicator.tsx
+++ components/ui/ScrollIndicator.tsx
@@ -9,46 +9,42 @@ interface ScrollIndicatorProps {
 
 export const ScrollIndicator = ({ className }: ScrollIndicatorProps) => {
     return (
-        <div className={cn("flex flex-col items-center", className)}>
-            <div className="relative w-7 h-11">
-                {/* Mouse Body Outline - Dual Tone */}
-                <svg
-                    width="28"
-                    height="44"
-                    viewBox="0 0 28 44"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="absolute inset-0"
-                >
-                    {/* Top Half Border - Darker */}
-                    <path
-                        d="M1 14C1 6.8203 6.8203 1 14 1C21.1797 1 27 6.8203 27 14V22H1V14Z"
-                        stroke="#4B5563"
-                        strokeWidth="2"
-                    />
-                    {/* Bottom Half Border - White */}
-                    <path
-                        d="M1 22V30C1 37.1797 6.8203 43 14 43C21.1797 43 27 37.1797 27 30V22H1Z"
-                        stroke="white"
-                        strokeWidth="2"
-                    />
-                </svg>
+        <div className={cn("flex flex-col items-center opacity-60", className)}>
+            <div className="relative w-6 h-10">
+                {/* Mouse Body Outline - Single Tone White */}
+                <div className="absolute inset-0 border-[1.5px] border-white rounded-full" />
 
-                {/* Scroll Dot */}
-                <div className="absolute inset-0 flex justify-center pt-5">
-                    <motion.div
-                        animate={{
-                            y: [0, 14, 0],
-                            opacity: [0, 1, 0],
-                            scale: [0.8, 1, 0.8],
-                        }}
-                        transition={{
-                            duration: 2,
-                            repeat: Infinity,
-                            ease: "easeInOut",
-                        }}
-                        className="w-1.5 h-1.5 rounded-full bg-gray-400"
-                    />
+                {/* Scroll Dot Animation - Trailing Effect */}
+                <div className="absolute inset-0 flex justify-center pt-2">
+                    <div className="relative h-full flex flex-col items-center">
+                        {/* Main Dot */}
+                        <motion.div
+                            animate={{
+                                y: [0, 16, 16],
+                                opacity: [0, 1, 0],
+                            }}
+                            transition={{
+                                duration: 2,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                            }}
+                            className="w-1.5 h-1.5 rounded-full bg-white"
+                        />
+                        {/* Trail Dot 1 */}
+                        <motion.div
+                            animate={{
+                                y: [4, 20, 20],
+                                opacity: [0, 0.4, 0],
+                            }}
+                            transition={{
+                                duration: 2,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                                delay: 0.1,
+                            }}
+                            className="w-1 h-1 rounded-full bg-white absolute top-0"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
